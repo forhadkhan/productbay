fd06a8cd0bf997ba5d398c7795e1cfee31b4cea2
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2026-01-19 13:20:40 +0600

    build: update admin assets

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => '4d6b176213cb24918844');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element'), 'version' => '08f11de3badb61c76433');
