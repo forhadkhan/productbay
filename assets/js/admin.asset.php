07997f53f31650c8ae0702d6c7023f59ee5cd3b8
Forhad Khan <67508944+forhadkhan@users.noreply.github.com>
2026-02-22 23:57:42 +0600

    build: update build files

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'a8a24697acb7a24de9b1');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b652f44210bdc36b1134');
---
+++ assets/js/runtime.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c95edf5964b7b9f02878', 'handle' => 'undefined-runtime');
