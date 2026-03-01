bbb38f9c8109e1a19773983bfd857e8363081e13
Forhad Khan <67508944+forhadkhan@users.noreply.github.com>
2026-03-02 03:33:48 +0600

    build: Remove lazy loading to generate single bundle JS output

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0f87f7ebe02de96ace26');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'ef5385787d7e351786d4');
