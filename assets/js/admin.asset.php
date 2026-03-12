d5451b9cb8db0c449d3846f75d7bbf4313dcbf83
Forhad Khan <forhadkhan@tuta.io>
2026-03-13 03:23:04 +0600

    chore: update gitignore and build files

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '73aa0870d9b638aa82c8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b0ed110e04be83136823');
