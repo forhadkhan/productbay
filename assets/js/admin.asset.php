caeb32818ed6ba7f128507e4e40e3ada818988ee
Forhad Khan <forhadkhan@tuta.io>
2026-03-17 02:56:02 +0600

    chore: update builds

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b0f6adb5c73a44eb0bad');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '816fe97800818eedf3ca');
