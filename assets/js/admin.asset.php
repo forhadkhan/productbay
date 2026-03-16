e1b06e00975fef9bb911ee04a0a7d0a214b0267d
Forhad Khan <forhadkhan@tuta.io>
2026-03-17 03:54:04 +0600

    feat: expose SettingsOption and SectionHeading for Pro

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '5d8889d3e842f92fa107');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'af311ffc167e5e230ea5');
