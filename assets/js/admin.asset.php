805b44a2e8bed56cfe6bf73496d56020b451f41f
Forhad Khan <forhadkhan@tuta.io>
2026-04-12 13:39:33 +0600

    chore: build assets update

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c4368eb1a78be3a7be1c');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '50e0b5e97edd758a1cee');
--- blocks/tab-product-table/index.asset.php
+++ blocks/tab-product-table/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '37340b80f5f50e5461f6');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e95903313aa413cb2a9b');
