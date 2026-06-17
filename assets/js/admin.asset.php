8d32eebb94ef7fdfa7ec9420e41b451c72ef261a
Forhad Khan <forhadkhan@tuta.io>
2026-06-17 11:22:49 +0600

    chore: add misc files

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'dc98e1aa9d77ef3ff2ab');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'a24b6d518d8ac45e9a40');
